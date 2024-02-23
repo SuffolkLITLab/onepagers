307b9634a712108527e5f114e05fea20baf92ff6
samglover <sam@samglover.net>
2024-02-22 23:11:24 -0600

    Update relative URLs.

--- assets/js/load-template-parts.js
+++ assets/js/load-template-parts.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
   let subtitle = $(docHeader).data('subtitle');
   let pdf = $(docHeader).data('pdf');
 
-  $(docHeader).load('../../template-parts/page-header.html', function () {
+  $(docHeader).load('template-parts/page-header.html', function () {
     $(docHeader).find('h1').text(title);
     if (subtitle) $(docHeader).find('.subtitle').text(subtitle);
     if ($('body').hasClass('one-pager')) {
-      $.get('../../template-parts/nav-buttons.html', function (navButtons) {
+      $.get('template-parts/nav-buttons.html', function (navButtons) {
         $(docHeader).append(navButtons);
         $(docHeader).promise().done(function () {
           if (pdf) {
@@ -21,7 +21,7 @@ $(document).ready(function () {
       });
     }
   });
-  $.get('../../template-parts/page-footer.html', function (pageFooterContent) {
+  $.get('template-parts/page-footer.html', function (pageFooterContent) {
     $(pageFooter).html(pageFooterContent);
     $('#document-container .page:not(:first-of-type)').append('<footer class="page-footer">' + pageFooterContent + '</footer>');
   });
